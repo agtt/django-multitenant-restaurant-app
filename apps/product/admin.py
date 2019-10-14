75b11f7bf13dca1ab832353dccc668a5c390ec6a
Agit <csd@hotmail.com.tr>
2019-10-14 21:13:27 +0100

    admin settings

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Category, Brand
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Brand)
